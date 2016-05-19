2275c0c92e19297cd09320e1a4b99635768bfa20
_ <git3@inf.ufpr.br>
2016-05-19 11:13:18 -0300

    Adicionando comentarios

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import ponto.py
 #comentario qualquer
+#adicionando comentario para o amigo que eu não conheço...
 main:
 	#print este e um supermercado
 	
@@ -10,3 +11,4 @@ caixa:
 	#ler o codigo de barra do produto
 	#calcular preco de cada produto	
 	#calcula o total a pagar
+	#não tem desconto????
